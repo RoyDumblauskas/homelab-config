ba258962ff9b98cd7a1467cd26a92f4cb937ad8f
Roy Dumblauskas <roydumblauskas@gmail.com>
2025-04-29 09:10:16 -0500

    declare sops key directory generate from ssh

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,7 +19,14 @@ in
 
   # Secret Management
   sops = {
-    age.keyFile = "/etc/sops/age/keys.txt";
+    # make sure that the age key is generated from the persisted host key
+    age = {
+      sshKeyPaths = [ "/persist/etc/ssh/ssh_host_ed25519_key" ];
+      keyFile = "/persist/var/lib/sops-nix/key.txt";
+      generateKey = true;
+    };
+
+    # I prefer to use json format for the secrets
     defaultSopsFormat = "json";
 
     secrets = {
