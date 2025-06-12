ec94cad235ba5265d9f4570a71e06b7d030cbe81
Roy Dumblauskas <roydumblauskas@gmail.com>
2025-06-12 18:17:16 -0500

    reintroduce services folder with nix updated

---
+++ homelab-services/minio-service/flake.nix
@@ -0,0 +1,105 @@
+{
+  description = "minio service for storing images with api access";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosModules.minio-service = { config, lib, pkgs, ... }:
+      let
+        opts = config.services.minio-service;
+      in {
+        options.services.minio-service = {
+          enable = lib.mkEnableOption "MinIO object storage server";
+
+          dataDir = lib.mkOption {
+            type = lib.types.path;
+            default = "/var/lib/minio";
+            description = "Directory to store MinIO data.";
+          };
+
+          dataPort = lib.mkOption {
+            type = lib.types.port;
+            default = 9000;
+            description = "MinIO S3 API port.";
+          };
+
+          consolePort = lib.mkOption {
+            type = lib.types.port;
+            default = 9001;
+            description = "MinIO Admin Console port.";
+          };
+
+          rootUser = lib.mkOption {
+            type = lib.types.str;
+            default = "minioadmin";
+            description = "MinIO root username.";
+          };
+
+          rootPasswordFile = lib.mkOption {
+            type = lib.types.path;
+            description = "File containing MinIO root password.";
+          };
+
+          default-nginx = {
+            enable = lib.mkEnableOption "Enable nginx reverse proxy for MinIO";
+            hostname = lib.mkOption {
+              type = lib.types.str;
+              default = "localhost";
+              description = "Hostname for nginx reverse proxy.";
+            };
+          };
+        };
+
+        config = lib.mkIf opts.enable {
+          users.groups.minio = {};
+          users.users.minio = {
+            isSystemUser = true;
+            group = "minio";
+            home = opts.dataDir;
+          };
+
+          systemd.services.minio = {
+            description = "MinIO S3-compatible object storage";
+            after = [ "network.target" ];
+            wantedBy = [ "multi-user.target" ];
+
+            serviceConfig = {
+              ExecStart = ''
+                ${pkgs.minio}/bin/minio server ${opts.dataDir} \
+                  --address ":${toString opts.dataPort}" \
+                  --console-address ":${toString opts.consolePort}"
+              ''; 
+              User = "minio";
+              Group = "minio";
+              Environment = [
+                "MINIO_ROOT_USER=${opts.rootUser}"
+                "MINIO_ROOT_PASSWORD_FILE=${opts.rootPasswordFile}"
+              ];
+              Restart = "always";
+            };
+          };
+
+          services.nginx = lib.mkIf opts.default-nginx.enable {
+            enable = true;
+            virtualHosts.${opts.default-nginx.hostname} = {
+              forceSSL = true;
+              enableACME = true;
+              locations."/" = {
+                proxyPass = "http://localhost:${toString opts.dataPort}";
+              };
+              locations."/console" = {
+                proxyPass = "https://localhost:${toString opts.consolePort}";
+              };
+            };
+          };
+
+          networking.firewall.allowedTCPPorts = lib.mkMerge [
+            [ opts.dataPort opts.consolePort ]
+            (lib.mkIf opts.default-nginx.enable [ 80 443 ])
+          ];
+        };
+      };
+  };
+}
