cca23c9577e7d3673f6006e537b4044a126402d0
Roy Dumblauskas <roydumblauskas@gmail.com>
2025-06-12 17:21:58 -0500

    udpate lock

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     quasiSecrets.url = "git+ssh://git@github.com/RoyDumblauskas/server-semi-secrets?shallow=1";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     disko = {
