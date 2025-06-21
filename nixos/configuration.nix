538e420df04ec0e239a3378bc88b0e9f2043bf0f
Roy Dumblauskas <roydumblauskas@gmail.com>
2025-06-21 12:19:55 -0500

    gamemode survival

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -105,7 +105,7 @@
     serverProperties = {
       server-port = 25565;
       difficulty = 3;
-      gamemode = 1;
+      gamemode = 0;
       max-players = 10;
       motd = "NixOS Minecraft server!";
       allow-cheats = true;
