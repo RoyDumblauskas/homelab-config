7c2eb0f6da9e8b5540e67d10f57afbbc70723d22
Roy Dumblauskas <roydumblauskas@gmail.com>
2025-06-20 21:54:23 -0500

    254

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -113,7 +113,7 @@
   networking = {
     hostName = meta.hostname;
     hostId = meta.hostId;
-    defaultGateway = "192.168.1.11";
+    defaultGateway = "192.168.1.254";
     nameservers = [ "1.1.1.1" "1.0.0.1" ];
 
     firewall = {
