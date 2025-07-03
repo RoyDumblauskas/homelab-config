a04d8fc3bd6a795bc4cc8003cf4df6bcf8a6118d
Roy Dumblauskas <roydumblauskas@gmail.com>
2025-07-03 15:36:34 -0500

    add proxy headers

--- homelab-services/minio-service/flake.nix
+++ homelab-services/minio-service/flake.nix
@@ -94,6 +94,14 @@
               # Add directive for s3like queries, which only accept root path allegedly
               locations."/" = {
                 proxyPass = "http://localhost:${toString opts.dataPort}";
+                extraConfig = ''
+                  proxy_set_header Host $host;
+                  proxy_set_header X-Real-IP $remote_addr;
+                  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                  proxy_set_header X-Forwarded-Proto $scheme;
+                  client_max_body_size 0;
+                  proxy_buffering off;
+                '';
               };
             };
           };
