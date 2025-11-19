9ccea72307f49db33cda6ece094c23343ae15edd
Roy Dumblauskas <roydumblauskas@gmail.com>
2025-11-19 02:29:31 -0600

    found the pg_hba.conf source

--- homelab-services/postgresql-db/flake.nix
+++ homelab-services/postgresql-db/flake.nix
@@ -66,20 +66,7 @@
           '';
 
           # allow remote connections to dev DBs
-          authentication = pkgs.lib.mkOverride 10 ''
-            # TYPE  DATABASE        USER            ADDRESS                 METHOD
-            # "local" is for Unix domain socket connections only
-            local   all             all                                     trust
-            # IPv4 local connections:
-            host    all             all             127.0.0.1/32            trust
-            # IPv6 local connections:
-            host    all             all             ::1/128                 trust
-            # Allow replication connections from localhost, by a user with the
-            # replication privilege.
-            local   replication     all                                     trust
-            host    replication     all             127.0.0.1/32            trust
-            host    replication     all             ::1/128                 trust
-            # Connections via LAN
+          authentication = ''
             ${lib.concatStringsSep " " (map (db: "host ${db}_dev all samenet md5\n") opts.databases) }
           '';
 
