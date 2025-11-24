cf618a86d32dc8b1f6a23736d7bb2fb11f278370
Roy Dumblauskas <roydumblauskas@gmail.com>
2025-11-23 19:52:45 -0600

    allow md5 connection to prod database

--- homelab-services/postgresql-db/flake.nix
+++ homelab-services/postgresql-db/flake.nix
@@ -71,7 +71,7 @@
 
             # Dev can be connected to via the LAN, Prod only via localhost
             ${lib.concatStringsSep " " (map (db: "host ${db}_dev ${db}_devuser samenet md5\n") opts.databases) }
-            ${lib.concatStringsSep " " (map (db: "local ${db} ${db}_produser md5\n") opts.databases) }
+            ${lib.concatStringsSep " " (map (db: "host ${db} ${db}_produser 127.0.0.1 md5\n") opts.databases) }
           '';
         };
 
