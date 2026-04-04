e1b5988a96a70c62429130f0a18e36ddbe3c9b4d
Roy Dumblauskas <roydumblauskas@gmail.com>
2026-04-04 15:47:04 -0500

    update package versions

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,14 +2,14 @@
   description = "Server Config Controller";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixvim = {
-      url = "github:nix-community/nixvim";
+      url = "github:nix-community/nixvim/nixos-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     quasiSecrets.url = "git+ssh://git@github.com/RoyDumblauskas/server-semi-secrets?shallow=1";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     disko = {
