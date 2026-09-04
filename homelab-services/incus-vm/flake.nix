375326db170616b89102f684ccfd143c0bafc398
Roy Dumblauskas <roydumblauskas@gmail.com>
2026-09-03 23:21:05 -0500

    add incus flake

---
+++ homelab-services/incus-vm/flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Flake to run services on a VM powered by incus";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs =
+    { ... }:
+    {
+
+    };
+}
