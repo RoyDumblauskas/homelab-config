a44e7ddc6ef36e3f5e4773aafb359101f2c27614
Roy Dumblauskas <roydumblauskas@gmail.com>
2026-09-17 22:00:40 -0500

    update inputs

--- test-vms/vm-test/flake.nix
+++ test-vms/vm-test/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    postgresql-db.url = "path:../homelab-services/postgresql-db";
+    postgresql-db.url = "path:../../homelab-services/postgresql-db";
   };
 
   outputs =
