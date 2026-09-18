e84e2a444c101ff0d8ff791a73d33f2092f01e72
Roy Dumblauskas <roydumblauskas@gmail.com>
2026-09-17 22:03:57 -0500

    update sops input

--- test-vms/vm-test/flake.nix
+++ test-vms/vm-test/flake.nix
@@ -4,12 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     postgresql-db.url = "path:../../homelab-services/postgresql-db";
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
       self,
       nixpkgs,
+      sops-nix,
       postgresql-db,
     }@inputs:
     {
@@ -19,6 +24,7 @@
           modules = [
             ./virtual.nix
             postgresql-db.nixosModules.postgresql-db
+            sops-nix.nixosModules.sops
 
             # declare the vm output
             "${inputs.nixpkgs}/nixos/modules/virtualisation/incus-virtual-machine.nix"
